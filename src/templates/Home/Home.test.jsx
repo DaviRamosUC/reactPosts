aa6d72001adffe665bba869ec4905e3cefd6b808
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-12-20 15:48:36 -0300

    Usando MSW para mockar dados de teste

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,5 +1,81 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import Home from '.';
+
+const handlers = [
+  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title 1',
+          body: 'body 1',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title 2',
+          body: 'body 2',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title 3',
+          body: 'body 3',
+        },
+      ]),
+    );
+  }),
+  rest.get('https://jsonplaceholder.typicode.com/photos', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          albumId: 1,
+          id: 1,
+          title: 'accusamus beatae ad facilis cum similique qui sunt',
+          url: 'https://via.placeholder.com/600/92c952',
+          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        },
+        {
+          albumId: 1,
+          id: 1,
+          title: 'accusamus beatae ad facilis cum similique qui sunt',
+          url: 'https://via.placeholder.com/600/92c952',
+          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        },
+        {
+          albumId: 1,
+          id: 1,
+          title: 'accusamus beatae ad facilis cum similique qui sunt',
+          url: 'https://via.placeholder.com/600/92c952',
+          thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+/* eslint-disable no-undef */
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render searchInput, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts =(');
+
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
     () => {
       handleLoadPosts(0, postPerPage);
     },
-    [handleLoadPosts],
+    [handleLoadPosts, postPerPage],
     postPerPage,
   );
 
@@ -66,87 +66,5 @@ export const Home = () => {
     </section>
   );
 };
-
-// export class Home2 extends Component {
-//   state = {
-//     posts: [],
-//     allPosts: [],
-//     page: 0,
-//     postPerPage: 12,
-//     searchValue: '',
-//   };
-
-//   async componentDidMount() {
-//     await this.loadPosts();
-//   }
-
-//   loadPosts = async () => {
-//     const { page, postPerPage } = this.state
-//     const postsAndPhotos = await loadPosts()
-//     this.setState({
-//       posts: postsAndPhotos.slice(page, postPerPage),
-//       allPosts: postsAndPhotos
-//     });
-//   }
-
-//   loadMorePosts = () => {
-//     const {
-//       page,
-//       postPerPage,
-//       allPosts,
-//       posts
-//     } = this.state
-//     const nextPage = page + postPerPage
-//     const nextPosts = allPosts.slice(nextPage, nextPage + postPerPage)
-//     posts.push(...nextPosts)
-//     this.setState({ posts, page: nextPage })
-//   }
-
-//   handleChange = (e) => {
-//     const { value } = e.target
-//     this.setState({ searchValue: value })
-//   }
-
-//   render() {
-//     const { posts, page, postPerPage, allPosts, searchValue } = this.state;
-//     const noMorePosts = page + postPerPage >= allPosts.length;
-
-//     const filteredPosts = !!searchValue ?
-//       allPosts.filter(post => {
-//         return post.title.toLowerCase().includes(searchValue.toLowerCase());
-//       })
-//       : posts;
-
-//     return (
-//       <section className="container">
-//         <div className="search-container">
-//           {!!searchValue && (
-//             <h3>Search Value: {searchValue}</h3>
-//           )}
-
-//           <TextInput searchValue={searchValue} handleChange={this.handleChange} />
-//         </div>
-
-//         {filteredPosts.length > 0 && (
-//           <Posts posts={filteredPosts} />
-//         )}
-
-//         {filteredPosts.length === 0 && (
-//           <p>Não existem posts =(</p>
-//         )}
-
-//         <div className="button-container">
-//           {!searchValue && (
-//             <Button
-//               text="Load more posts"
-//               onClick={this.loadMorePosts}
-//               disabled={noMorePosts}
-//             />
-//           )}
-//         </div>
-//       </section>
-//     );
-//   }
-// }
 
 export default Home;
